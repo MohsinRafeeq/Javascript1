e88dfb99f9c9e67b44ac101d9d3ed9df59d6ee6d
Mohsin Rafeeq <85779322+MohsinRafeeq@users.noreply.github.com>
2025-12-13 23:28:56 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"Use Strict" // treat all js code as newer version
+
+// alert( 3+3) // we are using nodejs, not browser
+
+console.log( 3+3); // code Readbiliy should be high
+
+console.log("Mohsin");
+
+let name = "Mohsin"
+let age = 23
+let isLoggedIn = true
+
+// number => 2 to power of 52
+// bigint
+// string
+// Boolean => True/Flase
+// null  standalone value 
+// undefined 
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null);  // object
+
+
+
+
+
+
+
