6a1660fa3e9a1398c57019d3bbc62d0c6c27b42c
Mohsin Rafeeq <85779322+MohsinRafeeq@users.noreply.github.com>
2025-12-14 00:17:53 +0000

    conversationsoperations

---
+++ 01_Basics/02_Conversationsoperations.js
@@ -0,0 +1,24 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1 
+
+let isLoggedIn ="Mohsin"
+let BooleanIsLoggedIn =  Boolean(isLoggedIn)
+//console.log(BooleanIsLoggedIn);
+
+// 1 => Ture ; 0 => false
+// "" => false 
+// "Mohsin" => True
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
